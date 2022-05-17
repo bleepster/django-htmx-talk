b6c98467d2ef9f1c5956c1e22049a21a31022c8d
bleepster <earl.lapus@gmail.com>
2022-05-18 02:54:49 +0800

    example 1: css improvements

--- example_1/example_1_2/polls/views.py
+++ example_1/example_1_2/polls/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-#from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from .models import Question, Choice
