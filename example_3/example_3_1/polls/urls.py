9d193bdbf6d64d473bc37b6b4aa3ddb2b44bb382
bleepster <earl.lapus@gmail.com>
2022-05-20 22:05:42 +0800

    remove unused views/urls

--- example_3/example_3_1/polls/urls.py
+++ example_3/example_3_1/polls/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path("login/", views.user_login, name="login"),
     path("logout/", views.user_logout, name="logout"),
     path("register/", views.register, name="register"),
-    path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results/", views.results, name="results"),
-    path("<int:question_id>/vote/", views.vote, name="vote"),
     path("<int:question_id>/choices/", views.get_choices, name="get_choices"),
 ]
--- example_3/example_3_1/polls/views.py
+++ example_3/example_3_1/polls/views.py
@@ -66,39 +66,12 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 @login_required
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {
-        "question": question,
-        "choices": [choice for choice in question.choice_set.all()],
-    }
-    return render(request, "polls/detail.html", context)
-
-@login_required
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {
         "question": question,
     }
     return render(request, "polls/results.html", context)
-
-@login_required
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    except (KeyError, Choice.DoesNotExist):
-        context = {
-            "question": question,
-            "error_message": "You didn't select a choice",
-        }
-        return render(request, "polls/detail.html", context)
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # See django documentation on why this should be a redirect
-        # https://docs.djangoproject.com/en/4.0/intro/tutorial04/#write-a-minimal-form
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
 @login_required
 def get_choices(request, question_id):
