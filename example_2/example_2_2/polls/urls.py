215042b072b89e6d1a3f62b45b8d54ea65f30bc6
bleepster <earl.lapus@gmail.com>
2022-05-21 00:52:31 +0800

    example 2 with htmx

---
+++ example_2/example_2_2/polls/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = "polls"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="logout"),
+    path("register/", views.register, name="register"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("username/", views.check_username, name="check_username"),
+]
